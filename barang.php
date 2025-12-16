fff92aa3faa158fc100982d25a8a14e0d3eea70e
kwangmin123455 <abdillah.kuhaku@gmail.com>
2025-12-16 11:14:41 +0800

    UI tabel barang

---
+++ barang.php
@@ -0,0 +1,42 @@
+<?php 
+require 'template/header.php';
+
+?>
+
+<!--**********************************
+            Content body start
+        ***********************************-->
+<div class="content-body">
+
+    <!-- row -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Data Barang Inventory Organisasi</h4>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered zero-configuration">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Tanggal Masuk</th>
+                                        <th>Stok</th>
+                                        <th>Kondisi</th>
+                                        <th>Keterangan</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Tiger Nixon</td>
+                                        <td>System Architect</td>
+                                        <td>Edinburgh</td>
+                                        <td>61</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                    </tr>
+
+                                    <?php require 'template/footer.php' 
+                                    ?>
--- template/header.php
+++ template/header.php
@@ -73,37 +73,7 @@
                         <span class="toggle-icon"><i class="icon-menu"></i></span>
                     </div>
                 </div>
-                <div class="header-right">
 
-                    <li class="icons dropdown">
-                        <div class="user-img c-pointer position-relative" data-toggle="dropdown">
-                            <span class="activity active"></span>
-                            <img src="images/user/1.png" height="40" width="40" alt="">
-                        </div>
-                        <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
-                            <div class="dropdown-content-body">
-                                <ul>
-                                    <li>
-                                        <a href="app-profile.html"><i class="icon-user"></i> <span>Profile</span></a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void()">
-                                            <i class="icon-envelope-open"></i> <span>Inbox</span>
-                                            <div class="badge gradient-3 badge-pill gradient-1">3</div>
-                                        </a>
-                                    </li>
-
-                                    <hr class="my-2">
-                                    <li>
-                                        <a href="page-lock.html"><i class="icon-lock"></i> <span>Lock Screen</span></a>
-                                    </li>
-                                    <li><a href="page-login.html"><i class="icon-key"></i> <span>Logout</span></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>
-                    </ul>
-                </div>
             </div>
         </div>
         <!--**********************************
@@ -118,13 +88,13 @@
                 <ul class="metismenu" id="menu">
                     <li class="nav-label">Dashboard</li>
                     <li>
-                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <a class="has-arrow" href="dashboard.php" aria-expanded="false">
                             <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
                         </a>
 
                     </li>
 
-                    <li class="has-arrow"><a href="#"><i class="icon-note menu-icon"></i><span
+                    <li class="has-arrow"><a href="barang.php"><i class="icon-note menu-icon"></i><span
                                 class="nav-text">Barang</span></a></li>
 
                     <li><a href="logOut.php" onclick="return confirm('Yakin mau Keluar ? ')"><i
